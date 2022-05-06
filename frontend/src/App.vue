df4032f596e3822a715c3ad4f5f94b9ae0228dcb
AGH <rlghks9252@gmail.com>
2022-05-06 14:36:33 +0900

    vue memo good

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <GroundMemo />
+  </div>
+</template>
+
+<script>
+import GroundMemo from "./components/GroundMemo.vue";
+export default {
+  components: {
+    GroundMemo,
+  },
+};
+</script>
---
+++ frontend/src/components/GroundMemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ground">
+    <HeaderMemo />
+    <ContentMemo />
+    <FooterMemo />
+  </div>
+</template>
+
+<script>
+import HeaderMemo from "./HeaderMemo.vue";
+import FooterMemo from "./FooterMemo.vue";
+import ContentMemo from "./ContentMemo.vue";
+
+export default {
+  components: { HeaderMemo, FooterMemo, ContentMemo },
+};
+</script>
